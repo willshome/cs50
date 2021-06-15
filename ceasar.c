27b190507cf7962a37833b380029ab218c7c9b07
Williams <william.home@hotmail.com>
2021-06-15 13:28:12 +0100

    Update ceasar.c
    Made code more concise - conditional in for loop and removed unnecessary "Success" print. 
    Int 'x' changed to clearer 'key'
    Print 'key' to confirm programme works
    Added comments

--- ceasar.c
+++ ceasar.c
@@ -4,30 +4,23 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    if (argc != 2) // confirm only one argument after ./ceasar vector
     {
-        printf("Usage: ./ceasar key\n");
+        printf("Usage: ./ceasar key\n"); // usage help
         return 1;
     }
     
-    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    for (int i = 0, n = strlen(argv[1]); i < n; i++) 
     {
-        if (isdigit(argv[1][i]))
+        if (isdigit(!argv[1][i])) // confirm only digit's in second vector
         {
-            if (i == strlen(argv[1])-1)
-            {
-                printf("Success\n%s\n", argv[1]);
-            }
-        }
-        else
-        {
-            printf("Usage: ./ceasar key\n");
+            printf("Usage: ./ceasar key\n"); // usage help
             return 1;
         }
     }
 
-    int x = atoi(argv[1]);
+    int key = atoi(argv[1]); // key for ciphertext 
+    printf("%i\n", key);
 }
